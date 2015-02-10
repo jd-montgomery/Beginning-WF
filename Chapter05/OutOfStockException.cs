4923e9f6e20073540c0c4ee737abb384f8c7f7b6
J.D. Montgomery <monty0018@gmail.com>
2015-02-09 23:00:00 -0600

    Chapter 06 - Exceptions

---
+++ Chapter05/OutOfStockException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter05
+{
+	public class OutOfStockException : Exception
+	{
+		public OutOfStockException()
+			: base()
+		{ }
+
+		public OutOfStockException(string message) : base(message)
+		{
+
+		}
+	}
+}
