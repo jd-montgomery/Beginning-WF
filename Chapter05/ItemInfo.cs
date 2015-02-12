2bf666ff0426d888246ffc99a4ccfd7de156faef
J.D. Montgomery <monty0018@gmail.com>
2015-02-11 23:13:28 -0600

    chapter 7
    custom activities

---
+++ Chapter05/ItemInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter05
+{
+	/// <summary>
+	/// Define the structure returned by the LookupItem custom activity
+	/// </summary>
+	public class ItemInfo
+	{
+		public string ItemCode { get; set; }
+		public string Description { get; set; }
+		public decimal Price { get; set; }
+	}
+}
